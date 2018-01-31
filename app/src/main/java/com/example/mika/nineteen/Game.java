1cd3d8170ab77136e188693cf5384174f64064d8
MaryoneFrolova <35322384+MaryoneFrolova@users.noreply.github.com>
2018-01-31 17:13:39 +1000

    step3
    engine

--- app/src/main/java/com/example/mika/nineteen/Game.java
+++ app/src/main/java/com/example/mika/nineteen/Game.java
@@ -2,6 +2,7 @@ package com.example.mika.nineteen;
 
 import android.util.Pair;
 
+import java.io.Console;
 import java.util.ArrayList;
 
 /**
@@ -15,21 +16,31 @@ public class Game {
 
     public void startGame (ArrayList<Integer> startMatrix, int sizeI) {
         this.sizeI = sizeI;
-
+        int i;
         ArrayList<Integer> thisRow = new ArrayList<Integer>();
-        for (int i = 0; i < startMatrix.size(); i += 1) {
+        for ( i = 0; i < startMatrix.size(); i += 1) {
             if (i % sizeI == 0 && i != 0) {
                 matrix.add(thisRow);
                 thisRow = new ArrayList<Integer>();
             }
             thisRow.add(startMatrix.get(i));
         }
+        matrix.add(thisRow);
+
     }
 
     public int stepGame (int i, int j, int ii, int jj) {
-        int res = 0;
 
-        if (j < jj) {
+
+        if (j > jj) {
+            int a = j;
+            j = jj;
+            jj = a;
+            a = i;
+            i = ii;
+            ii = a;
+        }
+        if (j == jj && i > ii) {
             int a = i;
             i = ii;
             ii = a;
@@ -37,56 +48,51 @@ public class Game {
             j = jj;
             jj = a;
         }
-        if (j == jj && i < ii) {
-            int a = i;
-            i = ii;
-            ii = a;
-            a = j;
-            j = jj;
-            jj = a;
-        }
 
-        if (!isNeighbor(i, j, ii, jj) || !isGood(i, j, ii, jj)) return res = 1;
+        boolean a = isNeighbor(i, j, ii, jj);
+        boolean b = isGood(i, j, ii, jj);
+        if (!(a && b))
+           {return 1;}
 
-        if (isAllNull()) return res = 2;
+        matrix.get(j).set(i, 0);
+        matrix.get(jj).set(ii, 0);
 
-        if (isNeedAdd()) return res = 3;
+        if (isAllNull()) return 3;
 
-        return res = 4;
+        if (isNeedAdd()) return 2;
+
+        return 4;
     }
 
 
     public boolean isNeighbor(int i, int j, int ii, int jj) {
-        if (case1(i, j, ii, jj) || case1(i, j, ii, jj) || case3(i, j, ii, jj))
-            return true;
-        return true;
+        if (i == ii)
+        {return case1(i, j, ii, jj);}
+        if (j == jj)
+        {return case2(i, j, ii, jj);}
+        return case3(i, j, ii, jj);
     }
 
     public boolean case1(int i, int j, int ii, int jj) {
-    if (i == ii) {
         for (int k = j + 1; k < jj; k += 1) {
             if (matrix.get(k).get(i) != 0 && k != jj) return false;
         }
         return true;
     }
-    return false;
-    }
 
     public boolean case2(int i, int j, int ii, int jj) {
-        if (j == jj) {
             for (int k = i + 1; k <= ii; k += 1) {
                 if (matrix.get(j).get(k) != 0 && k != ii) return false;
             }
             return true;
-        }
-        return false;
     }
 
     public boolean case3(int i, int j, int ii, int jj) {
         for (int l = j; l <= jj; l += 1) {
-            for (int k = (l == j) ? i + 1 : 0; k <= sizeI || (k == ii && l == jj); k += 1) {
-                if (matrix.get(l).get(k) != 0 && l != jj && k != ii)
-                    return false;
+            for (int k = (l == j ? i + 1 : 0); (k < sizeI); k += 1) {
+                if (l == jj && k == ii) return  true;
+                if (matrix.get(l).get(k) != 0) {
+                    return false;}
             }
         }
         return true;
@@ -105,22 +111,22 @@ public class Game {
     }
 
     public boolean  isSum (int i, int j, int ii, int jj) {
-        if (matrix.get(i).get(j) + matrix.get(ii).get(jj) == 10) return true;
+        if (matrix.get(j).get(i) + matrix.get(jj).get(ii) == 10) return true;
         return false;
     }
 
     public boolean isAllNull () {
-        for (int l = 0; l <= matrix.size(); l += 1)
-            for (int k = 0; k <= sizeI; k += 1)
+        for (int l = 0; l < matrix.size(); l += 1)
+            for (int k = 0; k < sizeI; k += 1)
                 if (matrix.get(l).get(k) != 0) return false;
         return true;
     }
 
     public boolean isNeedAdd () {
-        for (int l = 0; l <= matrix.size(); l += 1)
-            for (int k = 0; k <= sizeI; k += 1)
+        for (int l = 0; l < matrix.size(); l += 1)
+            for (int k = 0; k < sizeI; k += 1)
                 if (matrix.get(l).get(k) != 0)
-                    if (findGoodNeighbor(l, k)) return false;
+                    if (findGoodNeighbor(k, l)) return false;
         return true;
     }
 
@@ -130,7 +136,8 @@ public class Game {
     }
 
     public boolean findInLine (int i, int j) {
-        for (int l = i + 1; l <= sizeI; l += 1)
+        if (i == sizeI - 1) {return  false;}
+        for (int l = i + 1; l < sizeI; l += 1)
         {
            if (matrix.get(j).get(l) != 0) {
                if (isGood(i, j, l, j)) return true;
@@ -141,7 +148,8 @@ public class Game {
     }
 
     public boolean findInRow (int i, int j) {
-        for (int l = j + 1; l <= matrix.size(); l += 1)
+        if (j == matrix.size() - 1) return  false;
+        for (int l = j + 1; l < matrix.size(); l += 1)
         {
             if (matrix.get(l).get(i) != 0) {
                 if (isGood(i, j, i, l)) return true;
@@ -152,9 +160,10 @@ public class Game {
     }
 
     public boolean findNext (int i, int j) {
-        for (int l = j; l <= matrix.size(); l += 1)
+        for (int l = j; l < matrix.size(); l += 1)
         {
-            for (int k = (l == j ? i + 1 : 0); i <= sizeI; i += 1) {
+            if (i == sizeI - 1) continue;
+            for (int k = (l == j ? i + 1 : 0); i < sizeI; i += 1) {
                 if (matrix.get(l).get(k) != 0) {
                     if (isGood(i, j, l, k)) return true;
                 }
--- app/src/main/java/com/example/mika/nineteen/Main2Activity.java
+++ app/src/main/java/com/example/mika/nineteen/Main2Activity.java
@@ -1,11 +1,15 @@
 package com.example.mika.nineteen;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -37,7 +41,8 @@ public class Main2Activity extends AppCompatActivity {
         matrixButton = new ArrayList<ArrayList<ToggleButton>>();
 
         ArrayList<Integer> startArray = new ArrayList<Integer>();
-        startArray.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,1,1,1,2,1,3,1,4,1,5,1,6,1,7,1,8,1,9));
+        //startArray.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,1,1,1,2,1,3,1,4,1,5,1,6,1,7,1,8,1,9));
+        startArray.addAll(Arrays.asList(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1));
         a = new Game();
         a.startGame(startArray, sizeI);
         initGame(startArray);
@@ -46,8 +51,11 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     protected void addRow () {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
         TableLayout.LayoutParams tableParams = new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.WRAP_CONTENT);
-        TableRow.LayoutParams rowParams = new TableRow.LayoutParams(0, TableRow.LayoutParams.WRAP_CONTENT, 1.0f);
+        TableRow.LayoutParams rowParams = new TableRow.LayoutParams(0, height/15, 1.0f);
 
         tb = (TableLayout) findViewById(R.id.table_container);
         TableRow tr = new TableRow(this);
@@ -58,27 +66,76 @@ public class Main2Activity extends AppCompatActivity {
             final ToggleButton oneButton = new ToggleButton(this);
             oneButton.setLayoutParams(rowParams);
             oneButton.setChecked(false);
+            oneButton.setMinLines(1);
+
             oneButton.setId((j + 1) * 1000 + i);
             oneButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                 @Override
                 public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                     if (compoundButton.isChecked()) {
-                        if (check == 999) check = compoundButton.getId();
-                        Toast.makeText(getApplicationContext(), "click" + compoundButton.getId(), Toast.LENGTH_SHORT).show();
-
-                        if (check != 999) {
+                        if (check == 999)  {
+                            check = compoundButton.getId();
+                        }
+                        else if (check != 999) {
                             if (check == compoundButton.getId()) check = 999;
                             else {
                                 int current = compoundButton.getId();
                                 int res = a.stepGame(check % 1000, check / 1000, current % 1000, current / 1000);
                                 Toast.makeText(getApplicationContext(), "res" + res, Toast.LENGTH_SHORT).show();
+                                switch (res) {
+                                    case  1: {
+                                        matrixButton.get(check / 1000).get(check % 1000).setChecked(false);
+                                        check = compoundButton.getId();
+                                        break;
+                                    }
+                                    case 2: {
+                                        matrixButton.get(check / 1000).get(check % 1000).setBackgroundResource(R.drawable.cros);
+                                        matrixButton.get(check / 1000).get(check % 1000).setClickable(false);
+                                        matrixButton.get(check / 1000).get(check % 1000).setChecked(false);
+                                        compoundButton.setBackgroundResource(R.drawable.cros);
+                                        compoundButton.setChecked(false);
+                                        compoundButton.setClickable(false);
+                                        check = 999;
+                                        break;
+                                    }
+                                    case 3: {
+                                        matrixButton.get(check / 1000).get(check % 1000).setBackgroundResource(R.drawable.cros);
+                                        matrixButton.get(check / 1000).get(check % 1000).setClickable(false);
+                                        matrixButton.get(check / 1000).get(check % 1000).setChecked(false);
+                                        compoundButton.setBackgroundResource(R.drawable.cros);
+                                        compoundButton.setChecked(false);
+                                        compoundButton.setClickable(false);
+                                        check = 999;
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+                                        builder.setMessage("YOU ARE WIN!!!")
+                                                .setCancelable(false)
+                                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                                    public void onClick(DialogInterface dialog, int id) {
+                                                        //do things
+                                                    }
+                                                });
+                                        AlertDialog alert = builder.create();
+                                        alert.show();
+                                        break;
+                                    }
+                                    case 4: {
+                                        matrixButton.get(check / 1000).get(check % 1000).setBackgroundResource(R.drawable.cros);
+                                        matrixButton.get(check / 1000).get(check % 1000).setClickable(false);
+                                        matrixButton.get(check / 1000).get(check % 1000).setChecked(false);
+                                        compoundButton.setBackgroundResource(R.drawable.cros);
+                                        compoundButton.setChecked(false);
+                                        compoundButton.setClickable(false);
+                                        check = 999;
+                                        break;
+                                    }
+                                }
+
                             }
                         }
                     }
                     else {
                         if (check != 999) {
                             if (check == compoundButton.getId()) check = 999;}
-                        Toast.makeText(getApplicationContext(), "unclick" + compoundButton.getId(), Toast.LENGTH_SHORT).show();
                     }
                 }
             });
