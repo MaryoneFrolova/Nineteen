66e2205465b0b5a26d758e3875c4406bbaa2ebb2
MaryoneFrolova <35322384+MaryoneFrolova@users.noreply.github.com>
2018-01-31 19:34:49 +1000

    step 4

--- app/src/main/java/com/example/mika/nineteen/Game.java
+++ app/src/main/java/com/example/mika/nineteen/Game.java
@@ -13,6 +13,7 @@ public class Game {
 
     private ArrayList<ArrayList<Integer>> matrix = new ArrayList<ArrayList<Integer>>();
     private int sizeI = 9;
+    private ArrayList<Integer> whatAdd = new ArrayList<>();
 
     public void startGame (ArrayList<Integer> startMatrix, int sizeI) {
         this.sizeI = sizeI;
@@ -59,13 +60,16 @@ public class Game {
 
         if (isAllNull()) return 3;
 
-        if (isNeedAdd()) return 2;
+        if (isNeedAdd())
+        {
+            addNumbers();
+            return 2;}
 
         return 4;
     }
 
 
-    public boolean isNeighbor(int i, int j, int ii, int jj) {
+    private boolean isNeighbor(int i, int j, int ii, int jj) {
         if (i == ii)
         {return case1(i, j, ii, jj);}
         if (j == jj)
@@ -73,21 +77,21 @@ public class Game {
         return case3(i, j, ii, jj);
     }
 
-    public boolean case1(int i, int j, int ii, int jj) {
+    private boolean case1(int i, int j, int ii, int jj) {
         for (int k = j + 1; k < jj; k += 1) {
             if (matrix.get(k).get(i) != 0 && k != jj) return false;
         }
         return true;
     }
 
-    public boolean case2(int i, int j, int ii, int jj) {
+    private boolean case2(int i, int j, int ii, int jj) {
             for (int k = i + 1; k <= ii; k += 1) {
                 if (matrix.get(j).get(k) != 0 && k != ii) return false;
             }
             return true;
     }
 
-    public boolean case3(int i, int j, int ii, int jj) {
+    private boolean case3(int i, int j, int ii, int jj) {
         for (int l = j; l <= jj; l += 1) {
             for (int k = (l == j ? i + 1 : 0); (k < sizeI); k += 1) {
                 if (l == jj && k == ii) return  true;
@@ -98,31 +102,31 @@ public class Game {
         return true;
     }
 
-    public boolean isGood (int i, int j, int ii, int jj) {
+    private boolean isGood (int i, int j, int ii, int jj) {
         if (isEqual(i, j, ii, jj) || isSum(i, j, ii, jj)) {
             return true;
         }
         return false;
     }
 
-    public boolean  isEqual (int i, int j, int ii, int jj) {
+    private boolean  isEqual (int i, int j, int ii, int jj) {
         if (matrix.get(j).get(i) == matrix.get(jj).get(ii)) return true;
         return false;
     }
 
-    public boolean  isSum (int i, int j, int ii, int jj) {
+    private boolean  isSum (int i, int j, int ii, int jj) {
         if (matrix.get(j).get(i) + matrix.get(jj).get(ii) == 10) return true;
         return false;
     }
 
-    public boolean isAllNull () {
+    private boolean isAllNull () {
         for (int l = 0; l < matrix.size(); l += 1)
-            for (int k = 0; k < sizeI; k += 1)
+            for (int k = 0; k < matrix.get(l).size(); k += 1)
                 if (matrix.get(l).get(k) != 0) return false;
         return true;
     }
 
-    public boolean isNeedAdd () {
+    private boolean isNeedAdd () {
         for (int l = 0; l < matrix.size(); l += 1)
             for (int k = 0; k < sizeI; k += 1)
                 if (matrix.get(l).get(k) != 0)
@@ -130,46 +134,86 @@ public class Game {
         return true;
     }
 
-    public boolean findGoodNeighbor (int i, int j) {
+    private boolean findGoodNeighbor (int i, int j) {
         if (findInLine(i, j) || findInRow(i, j) || findNext(i, j))  return true;
         else return false;
     }
 
-    public boolean findInLine (int i, int j) {
+    private boolean findInLine (int i, int j) {
         if (i == sizeI - 1) {return  false;}
         for (int l = i + 1; l < sizeI; l += 1)
         {
            if (matrix.get(j).get(l) != 0) {
                if (isGood(i, j, l, j)) return true;
+               else return false;
            }
 
         }
         return false;
     }
 
-    public boolean findInRow (int i, int j) {
+    private boolean findInRow (int i, int j) {
         if (j == matrix.size() - 1) return  false;
         for (int l = j + 1; l < matrix.size(); l += 1)
         {
             if (matrix.get(l).get(i) != 0) {
                 if (isGood(i, j, i, l)) return true;
+                else return false;
             }
 
         }
         return false;
     }
 
-    public boolean findNext (int i, int j) {
+    private boolean findNext (int i, int j) {
         for (int l = j; l < matrix.size(); l += 1)
         {
             if (i == sizeI - 1) continue;
-            for (int k = (l == j ? i + 1 : 0); i < sizeI; i += 1) {
+            for (int k = (l == j ? i + 1 : 0); k < sizeI; k += 1) {
                 if (matrix.get(l).get(k) != 0) {
-                    if (isGood(i, j, l, k)) return true;
+                    if (isGood(i, j, k, l)) return true;
+                    else return false;
                 }
             }
         }
         return false;
     }
 
+    private void addNumbers () {
+        whatAdd.removeAll(whatAdd);
+
+        for (int j = 0; j < matrix.size(); j += 1) {
+            for (int i = 0; i < matrix.get(j).size(); i += 1) {
+                if (matrix.get(j).get(i) != 0)
+                    whatAdd.add(matrix.get(j).get(i));
+            }
+        }
+
+        int indexY = matrix.size();
+        int indexX = matrix.get(indexY - 1).size();
+        int i;
+
+        ArrayList<Integer> thisRow = new ArrayList<Integer>();;
+        for ( i = 0; i < whatAdd.size(); i += 1) {
+
+
+            if ((indexX + i) % sizeI == 0) {
+                if (indexX + i == sizeI){
+
+                }
+                else {
+                    matrix.add(thisRow);
+                    thisRow = new ArrayList<Integer>();
+                }
+            }
+            if (indexX + i < sizeI - 1) matrix.get(indexY).get(whatAdd.get(i));
+            else thisRow.add(whatAdd.get(i));
+        }
+        matrix.add(thisRow);
+
+    }
+
+    public ArrayList<Integer> getWhatAdd() {
+        return whatAdd;
+    }
 }
--- app/src/main/java/com/example/mika/nineteen/Main2Activity.java
+++ app/src/main/java/com/example/mika/nineteen/Main2Activity.java
@@ -24,11 +24,14 @@ public class Main2Activity extends AppCompatActivity {
 
     TableLayout tb;
     ArrayList<ArrayList<ToggleButton>> matrixButton;
+    ArrayList<Integer> whatToAdd;
     int sizeI = 9;
     Game a;
     int j = -1;
     int l = 0;
     int  check = 999;
+
+
 
 
     @Override
@@ -41,8 +44,8 @@ public class Main2Activity extends AppCompatActivity {
         matrixButton = new ArrayList<ArrayList<ToggleButton>>();
 
         ArrayList<Integer> startArray = new ArrayList<Integer>();
-        //startArray.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,1,1,1,2,1,3,1,4,1,5,1,6,1,7,1,8,1,9));
-        startArray.addAll(Arrays.asList(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1));
+        startArray.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,1,1,1,2,1,3,1,4,1,5,1,6,1,7,1,8,1,9));
+        //startArray.addAll(Arrays.asList(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1));
         a = new Game();
         a.startGame(startArray, sizeI);
         initGame(startArray);
@@ -60,7 +63,7 @@ public class Main2Activity extends AppCompatActivity {
         tb = (TableLayout) findViewById(R.id.table_container);
         TableRow tr = new TableRow(this);
         tr.setLayoutParams(tableParams);
-
+        j = matrixButton.size() - 1;
         ArrayList<ToggleButton> rowButton = new ArrayList<ToggleButton>();
         for (int i = 0; i < sizeI; i += 1) {
             final ToggleButton oneButton = new ToggleButton(this);
@@ -81,7 +84,6 @@ public class Main2Activity extends AppCompatActivity {
                             else {
                                 int current = compoundButton.getId();
                                 int res = a.stepGame(check % 1000, check / 1000, current % 1000, current / 1000);
-                                Toast.makeText(getApplicationContext(), "res" + res, Toast.LENGTH_SHORT).show();
                                 switch (res) {
                                     case  1: {
                                         matrixButton.get(check / 1000).get(check % 1000).setChecked(false);
@@ -96,6 +98,8 @@ public class Main2Activity extends AppCompatActivity {
                                         compoundButton.setChecked(false);
                                         compoundButton.setClickable(false);
                                         check = 999;
+                                        whatToAdd = a.getWhatAdd();
+                                        addElements();
                                         break;
                                     }
                                     case 3: {
@@ -106,16 +110,7 @@ public class Main2Activity extends AppCompatActivity {
                                         compoundButton.setChecked(false);
                                         compoundButton.setClickable(false);
                                         check = 999;
-                                        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
-                                        builder.setMessage("YOU ARE WIN!!!")
-                                                .setCancelable(false)
-                                                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                                    public void onClick(DialogInterface dialog, int id) {
-                                                        //do things
-                                                    }
-                                                });
-                                        AlertDialog alert = builder.create();
-                                        alert.show();
+                                        Toast.makeText(getApplicationContext(), "YOU WIN!!!" , Toast.LENGTH_SHORT).show();
                                         break;
                                     }
                                     case 4: {
@@ -173,5 +168,52 @@ public class Main2Activity extends AppCompatActivity {
                 o += 1;
             }
         }
+    }
+
+    private void addElements () {
+        int indexY = matrixButton.size();
+        int indexX = matrixButton.get(indexY - 1).size();
+        int i;
+        int index = 0;
+
+        for ( i = 0; i < whatToAdd.size(); i += 1, index += 1) {
+
+
+            if ((indexX + i) % sizeI == 0) {
+                index = 0;
+                if (indexX + i == sizeI){
+                    if (indexX + whatToAdd.size() > sizeI - 1) addRow();
+                }
+                else {
+                    addRow();
+                }
+            }
+            if (indexX + i < sizeI - 1) {
+                matrixButton.get(indexY).get(indexX + i).setClickable(true);
+                matrixButton.get(indexY).get(indexX + i).setText(String.valueOf(whatToAdd.get(i)));
+                matrixButton.get(indexY).get(indexX + i).setTextOn(String.valueOf(whatToAdd.get(i)));
+                matrixButton.get(indexY).get(indexX + i).setTextOff(String.valueOf(whatToAdd.get(i)));
+            }
+            else {
+                ToggleButton oneBtn = matrixButton.get(matrixButton.size()-1).get(index);
+                oneBtn.setText(String.valueOf(whatToAdd.get(i)));
+                oneBtn.setTextOn(String.valueOf(whatToAdd.get(i)));
+                oneBtn.setTextOff(String.valueOf(whatToAdd.get(i)));
+            }
+        }
+
+        int o = (i + indexX) % sizeI;
+        if (i % sizeI != 0) {
+            while (o < sizeI) {
+                ToggleButton oneBtn = matrixButton.get(matrixButton.size()-1).get(o);
+                oneBtn.setClickable(false);
+                oneBtn.setText("");
+                oneBtn.setTextOn("");
+                oneBtn.setTextOff("");
+                o += 1;
+            }
+        }
+
+
     }
 }
